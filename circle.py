d2482da9e98f4cb489d2792632c673775e16c6bf
Tanuja-0620 <75062188+Tanuja-0620@users.noreply.github.com>
2021-04-21 04:37:36 +0530

    task12.py

--- circle.py
+++
@@ -1,3 +0,0 @@
-r = float(input("Enter radius of circle: "))
-a = 3.14159 * r * r
-print("Area of circle =", a)
---
+++ task12.py
@@ -0,0 +1,10 @@
+task 1:
+r = float(input("Enter radius of circle: "))
+a = 3.14159 * r * r
+print("Area of circle =", a)
+task 2:
+  filename = input("Input the filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
+>>> filename = abc.py
+>>>extension of the file = Python
